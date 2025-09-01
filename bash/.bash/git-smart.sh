bc1f011fa047a55d681c0fc4d797e3a524f12a55
Alexander Ames <Alexander.Ames@gmail.com>
2025-09-01 13:34:50 -0700

    Changed windows filesystem detection

--- bash/.bash/git-smart.sh
+++ bash/.bash/git-smart.sh
@@ -3,7 +3,7 @@
 # Detect filesystem type of current directory
 fs_type=$(stat -f -c %T .)
 
-if [[ "$fs_type" == "9p" && -x "$windows_git" ]]; then
+if [[ "$fs_type" == "9p" || "$fs_type" == "v9fs" ]]; then
   # On Windows filesystem: use Windows Git
   cmd.exe /c git "$@"
 else
